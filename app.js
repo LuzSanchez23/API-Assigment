3b0c4d7d8ac1423256b8e67cae2162a69dd41125
Luz Sanchez <nena_baby23@hotmail.com>
2022-01-18 17:58:52 -0700

    starting a new contenttt

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
-//mapquest key: 	i9JTL9YIvAvksDSCDCxwG5oOA54PGap7
-
-const apiUrl = "//open.mapquestapi.com/directions/v2/route?key=i9JTL9YIvAvksDSCDCxwG5oOA54PGap7&from=Parker Blvd,Centennial,CO&to=2400+S+Glebe+Rd,+Arlington,+VA"
-
-fetch (apiURL)
-.then((response) => response.json())
-.then((mapInfo) => {
-    console.log(mapInfo);
-
-    document.getElementById("place").innerHTML=weatherInfo.name;
-    document.getElementById("currentTemp").innerHTML=weatherInfo.main.temp;
-    document.getElementById("feels").innerHTML=weatherInfo.main.feels_like;
-    document.getElementById("windSped").innerHTML=weatherInfo.wind.speed;
-    document.getElementById("humid").innerHTML=weatherInfo.main.humidity;
-    document.getElementById("sunny").innerHTML=weatherInfo.sys.sunrise;
-
-    const iconcode= weatherInfo.weather[0].icon;
-    console.log(iconcode); //optional to test code
-    const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
-    console.log(icon_path); //optional to test code
-
-    document.getElementById("weatherIcon").src= icon_path;
-    //document.getElementById("sun").innerHTML=weatherInfo.sys.sunrise;
-    
-});
+var books = [
+    {
+        tittle: 'The Design of EveryDay Things',
+        author: 'Don Norman',
+        alreadyREadd: false
+    }, {
+        tittle: 'TheM Most Human Human',
+        author: 'Brian Christian',
+        alreadyREad: true
+    }
+];
+//interate means for loop in this case the books lenght
+for (var i=0; i < books.length; i++) {
+    //to create a element is: document.createElement(p) porque es un paragraph
+    var bookP = document.createElement('p');
+    var bookDescription = document.createTextNode(books[i].tittle + ' by' + books[i].author);
+    bookP.appendChild(bookDescription);
+    document.body.appendChild(bookP);
+}
